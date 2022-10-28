daaa6e638f73a6b776d211e08f61e60c0adff113
James Ferlez <jferlez@gmail.com>
2022-10-28 11:56:08 -0700

    Update lint float command

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -63,25 +63,25 @@ local custom_attach = function(client, bufnr)
   --   end,
   -- })
 
-  vim.api.nvim_create_user_command("LF", 
+  api.nvim_create_user_command("LF", 
     function()
-      local opts = {
+      local float_opts = {
         focusable = false,
         close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-        border = 'rounded',
-        source = 'always',  -- show source in diagnostic popup window
-        prefix = ' '
+        border = "rounded",
+        source = "always", -- show source in diagnostic popup window
+        prefix = " ",
       }
 
       if not vim.b.diagnostics_pos then
         vim.b.diagnostics_pos = { nil, nil }
       end
 
-      local cursor_pos = vim.api.nvim_win_get_cursor(0)
-      if (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2]) and
-        #vim.diagnostic.get() > 0
+      local cursor_pos = api.nvim_win_get_cursor(0)
+      if (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
+          and #diagnostic.get() > 0
       then
-          vim.diagnostic.open_float(nil, opts)
+        diagnostic.open_float(nil, float_opts)
       end
 
       vim.b.diagnostics_pos = cursor_pos
